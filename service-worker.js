60d9b4066196187070073940632d1f4a039ffe0b
Sayaz06 <adha86614@gmail.com>
2026-03-19 01:58:11 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,9 +3,20 @@ self.addEventListener("install", (e) => {
 });
 
 self.addEventListener("activate", (e) => {
-  clients.claim();
+  e.waitUntil(clients.claim());
 });
 
 self.addEventListener("fetch", (e) => {
-  e.respondWith(fetch(e.request));
+  // 1. Tapis: Hanya layan url bermula dengan http atau https sahaja
+  if (!(e.request.url.startsWith('http:') || e.request.url.startsWith('https:'))) {
+    return; 
+  }
+
+  // 2. Tambah sistem 'catch' kalau fetch gagal
+  e.respondWith(
+    fetch(e.request).catch((error) => {
+      console.log("Terlepas pandang/gagal fetch:", e.request.url);
+      // Ralat takkan jadi merah lagi, ia cuma log biasa
+    })
+  );
 });
